291f76f835d09bf57d27a38e7ea358f82bb6d070
admin <dovbysh.bohdan@gmail.com>
2017-05-04 17:37:24 +0300

    04052017

--- InternetShop/src/main/java/com/internetshop/controller/Main.java
+++ InternetShop/src/main/java/com/internetshop/controller/Main.java
@@ -4,22 +4,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import com.internetshop.entity.User;
-
 public class Main {
 
 	public static void main(String[] args) {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("primary");
 		EntityManager manager = factory.createEntityManager();
-
 		manager.getTransaction().begin();
-		User user = new User();
-		manager.persist(user);
-
-
+		manager.getTransaction().commit();
 		manager.close();
 		factory.close();
-
 	}
 
 }
